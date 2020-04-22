ac4807d87085bb5368a413fc023d3c9f06beb1bf
yaroslav <slavko.marchuk.1998@gmail.com>
2020-04-22 23:09:20 +0300

    Add fields id, datetime.

--- sel/driver.py
+++ sel/driver.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 import pika
@@ -40,8 +41,8 @@ def get_data() -> None:
         for i in elem_table:
             elem_table_title = i.find_element_by_xpath(TITLE_TAG)
             elem_table_author = i.find_element_by_xpath(AUTHOR_TAG)
-            elem_table_views = ''.join(i.find_elements_by_xpath(VIEWS_TAG))
             elem_table_href = i.find_element_by_link_text(f"{elem_table_title.text}").get_attribute("href")
+            elem_table_date = datetime.datetime.now().strftime("%x")
             if article_count == 1 and not last_news:
                 last_news = elem_table_title.text
             elif last_news == elem_table_title.text:
@@ -50,14 +51,14 @@ def get_data() -> None:
                 first_article = elem_table_title.text
             channel.basic_publish(exchange='',
                                   routing_key=RABBIT_QUEUE,
-                                  body='|'.join([elem_table_title.text, elem_table_author.text,
-                                             elem_table_views, elem_table_href]))
+                                  body='|'.join([str(article_count), elem_table_title.text, elem_table_author.text,
+                                                 elem_table_href, elem_table_date]))
             print(f'\t{article_count} {elem_table_title.text}')
             print(f'\tlast_news={last_news}')
             print(f'\tfirst_article={first_article}')
             print(f'\t{elem_table_author.text}')
-            print(f'\t{elem_table_views}')
             print(f'\t{elem_table_href}')
+            print(f'\t{elem_table_date}')
             article_count += 1
         print(f'first_article={first_article}')
         if first_article:
